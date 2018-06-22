f9eda091802a7cf025d07ab8ecebb9259fcd8129
Harrison Beckerich <hbeckeri@purdue.edu>
2018-06-22 10:41:30 -0400

    master updates

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const schema = [
 	{ name: 'price', alias: 'p', type: String },
 	{ name: 'wallet', alias: 'w', type: String, defaultValue: persistantArgs.wallet },
 	{ name: 'symbol', alias: 's', type: String, defaultValue: persistantArgs.symbol },
-	{ name: 'exchange', alias: 'e', type: String },
+	{ name: 'exchange', alias: 'e', type: String, defaultValue: persistantArgs.exchange },
 	{ name: 'amount', alias: 'a', type: String },
 	{ name: 'address', type: String },
 	{ name: 'order', type: String },
@@ -35,140 +35,162 @@ const usageSchema = [
 		content: 'CLI tool for crypto'
 	},
 	{
-		header: 'Commands',
+		header: 'Wallet Commands',
 		content: [
 			{
-				name: '[bold]{balance}',
+				name: '{bold balance}',
 				label: 'See your current balance'
 			},
 			{
-				name: '[bold]{deposit}',
-				label: 'Deposit funds'
-			},
-			{
-				name: '[bold]{send}',
+				name: '{bold send}',
 				label: 'Send funds to an address'
 			},
 			{
-				name: '[bold]{address}',
+				name: '{bold address}',
 				label: 'Get a deposit address'
 			},
 			{
-				name: '[bold]{buy}',
-				label: 'Place a buy order'
+				name: '{bold compile}',
+				label: 'Compile an ethereum smart contract (Ethereum Only)'
 			},
 			{
-				name: '[bold]{sell}',
-				label: 'Place a sell order'
-			},
-			{
-				name: '[bold]{bid}',
-				label: 'See the current bid price'
-			},
-			{
-				name: '[bold]{ask}',
-				label: 'See the current ask price'
-			},
-			{
-				name: '[bold]{cancelOrder}',
-				label: 'Cancel an open order'
-			},
-			{
-				name: '[bold]{cancel}',
-				label: 'Cancel all open orders'
-			},
-			{
-				name: '[bold]{openOrders}',
-				label: 'Fetch open orders'
-			},
-			{
-				name: '[bold]{closedOrders}',
-				label: 'Fetch closed orders'
-			},
-			{
-				name: '[bold]{autoLong}',
-				label: 'Go long at a set price protected by stops'
-			},
-			{
-				name: '[bold]{autoShort}',
-				label: 'Go short at a set price protected by stops'
-			},
-			{
-				name: '[bold]{compile}',
-				label: 'Compile an ethereum smart contract'
-			},
-			{
-				name: '[bold]{deploy}',
-				label: 'Deploy an ethereum smart contract'
+				name: '{bold deploy}',
+				label: 'Deploy an ethereum smart contract (Ethereum Only)'
 			}
 		]
 	},
 	{
-		header: 'Options',
-		optionList: [
+		header: 'Exchange Commands',
+		content: [
 			{
-				name: 'exchange',
-				typeLabel: '[underline]{exchange}',
-				description: 'The exchange to use.'
+				name: '{bold buy}',
+				label: 'Place a buy order'
 			},
 			{
-				name: 'price',
-				typeLabel: '[underline]{price}',
-				description: 'The price to buy/sell at. Defaults to the current market bid/ask.'
+				name: '{bold sell}',
+				label: 'Place a sell order'
+			},
+			{
+				name: '{bold bid}',
+				label: 'See the current bid price'
+			},
+			{
+				name: '{bold ask}',
+				label: 'See the current ask price'
+			},
+			{
+				name: '{bold cancelOrder}',
+				label: 'Cancel an open order'
+			},
+			{
+				name: '{bold openOrders}',
+				label: 'Fetch open orders'
+			},
+			{
+				name: '{bold closedOrders}',
+				label: 'Fetch closed orders'
+			},
+			{
+				name: '{bold autoLong}',
+				label: 'Go long at a set price protected by stops (Bitmex Only)'
+			},
+			{
+				name: '{bold autoShort}',
+				label: 'Go short at a set price protected by stops (Bitmex Only)'
+			}
+		]
+	},
+	{
+		header: 'Wallet Options',
+		optionList: [
+			{
+				name: 'wallet',
+				typeLabel: '{underline wallet}',
+				description: 'The wallet to use.'
 			},
 			{
 				name: 'amount',
-				typeLabel: '[underline]{amount}',
-				description: 'The amount to buy/sell. Defaults to full balance.'
+				typeLabel: '{underline amount}',
+				description: 'The amount of coin to use'
 			},
 			{
 				name: 'address',
-				typeLabel: '[underline]{address}',
-				description: 'The address to withdraw to'
+				typeLabel: '{underline address}',
+				description: 'The address to use'
+			},
+			{
+				name: 'contract',
+				typeLabel: '{underline contract}',
+				description: 'The path to a solidity smart contract (Ethereum Only)'
+			},
+			{
+				name: 'abi',
+				typeLabel: '{underline abi}',
+				description: 'The path to a compiled solidity abi (Ethereum Only)'
+			},
+			{
+				name: 'bytecode',
+				typeLabel: '{underline bytecode}',
+				description: 'The path to a compiled solidity bytecode (Ethereum Only)'
+			},
+			{
+				name: 'gasPrice',
+				typeLabel: '{underline gasPrice}',
+				description: 'The price, in gwei, for an ethereum transaction (Ethereum Only)'
+			},
+			{
+				name: 'gasLimit',
+				typeLabel: '{underline gasLimit}',
+				description: 'The limit, in mwei, for an ethereum transaction (Ethereum Only)'
+			},
+			{
+				name: 'help',
+				description: 'Print this usage guide'
+			}
+		]
+	},
+	{
+		header: 'Exchange Options',
+		optionList: [
+			{
+				name: 'exchange',
+				typeLabel: '{underline exchange}',
+				description: 'The symbol to trade against'
+			},
+			{
+				name: 'symbol',
+				typeLabel: '{underline exchange}',
+				description: 'The symbol to trade with'
+			},
+			{
+				name: 'price',
+				typeLabel: '{underline price}',
+				description: 'The price to buy/sell at. Defaults to the current market bid/ask'
+			},
+			{
+				name: 'amount',
+				typeLabel: '{underline amount}',
+				description: 'The amount to buy/sell. Defaults to full balance'
 			},
 			{
 				name: 'order',
-				typeLabel: '[underline]{order}',
+				typeLabel: '{underline order}',
 				description: 'The id of the order'
 			},
 			{
 				name: 'contract',
-				typeLabel: '[underline]{contract}',
+				typeLabel: '{underline contract}',
 				description: 'The path to a solidity smart contract'
 			},
 			{
-				name: 'abi',
-				typeLabel: '[underline]{abi}',
-				description: 'The path to a compiled solidity abi'
-			},
-			{
-				name: 'bytecode',
-				typeLabel: '[underline]{bytecode}',
-				description: 'The path to a compiled solidity bytecode'
-			},
-			{
-				name: 'gasPrice',
-				typeLabel: '[underline]{gasPrice}',
-				description: 'The price, in gwei, for an ethereum transaction'
-			},
-			{
-				name: 'gasLimit',
-				typeLabel: '[underline]{gasLimit}',
-				description: 'The limit, in gwei, for an ethereum transaction'
-			},
-			{
 				name: 'risk',
-				typeLabel: '[underline]{risk}',
+				typeLabel: '{underline risk}',
 				description: 'The percent risk to take in an auto long/short'
 			},
 			{
 				name: 'reward',
-				typeLabel: '[underline]{reward}',
-				description: 'The percent reward to take in an auto long/short.'
-			},
-			{
-				name: 'help',
-				description: 'Print this usage guide.'
+				typeLabel: '{underline reward}',
+				description: 'The percent reward to take in an auto long/short'
 			}
 		]
 	}
